b206d3fa7832467bb6b90defc8fc63177f6b6090
Toma <ragimova-tamara@mail.ru>
2023-05-20 19:45:59 +0300

    add main page

---
+++ frontend/src/Components/MenuList/MenuList.jsx
@@ -0,0 +1,23 @@
+import s from "./MenuList.module.css";
+
+const MenuList = ({ menuItems }) => {
+  return (
+    <div className={s.menuList}>
+      {menuItems.map((item) => (
+        <MenuItem img={item.img} price={item.price} title={item.title} />
+      ))}
+    </div>
+  );
+};
+
+export default MenuList;
+
+const MenuItem = ({ img, title, price }) => {
+  return (
+    <div className={s.menuItem}>
+      <img src={img} alt={title} />
+      <h2 className={s.menuItemTitle}>{title}</h2>
+      <div>{`${price} ₽`}</div>
+    </div>
+  );
+};
